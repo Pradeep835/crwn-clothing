6390a018c44d0aae261a64f59eea812397c55f90
Pradeep835 <45381766+Pradeep835@users.noreply.github.com>
2022-09-13 00:37:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import Directory from "./components/directory/directory.component";
+
+
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+
+  return (
+    //Instead of declaring 5 seperate divs what we have done here is that
+    //we are iterating through array of categories object and returned the
+    //jsx element which will give us a new element or div everytime we iterate
+    //through it.
+    <Directory categories={categories}/>
+  );
+};
+
+export default App;
